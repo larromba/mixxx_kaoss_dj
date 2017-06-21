add649c3e8b2cc7197f1b681578fb68fa0423e5e
Fayaaz Ahmed <fayaaz.ahmed@gmail.com>
2017-06-21 01:45:50 +0100

    Got jogwheels working somewhat

--- Korg-KAOSS-DJ-scripts.js
+++ Korg-KAOSS-DJ-scripts.js
@@ -1,25 +1,51 @@
-var KAOSSDJ = {};
+function KAOSSDJ() {};
+
+KAOSSDJ();
+
+KAOSSDJ.deck = function(decknum) {
+    this.decknum = decknum;
+    this.group = "[Channel" + decknum + "]";
+    this.loaded = false;
+    this.jogWheelsInScratchMode = false;
+    this.PADMode = false; //false = not pressed; true = pressed
+    this.shiftKey = false;
+    this.touch = false;
+
+};
+
+
+KAOSSDJ.currentDeck = function(channel){
+   script.midiDebug(channel);
+   var chann = (channel - 6);
+   print(chann);
+   return chann;
+};
 
 KAOSSDJ.wheelTouch = function (channel, control, value, status, group) {
-    if ((status & 0xF0) === 0x90) {    // If button down
-        var alpha = 1.0/8;
-        var beta = alpha/32;
-        engine.scratchEnable(KAOSSDJ.currentDeck, 128, 33+1/3, alpha, beta);
-    } else {    // If button up
-        engine.scratchDisable(KAOSSDJ.currentDeck);
-    }
-}
- 
+    var alpha = 1.0/8;
+    var beta = alpha/32;
+    var deckno = channel - 6
+    print ('Deck num: ' + deckno);
+
+    if (value === 0X7F) {
+        engine.scratchEnable(deckno, 128, 33+1/3, alpha, beta);
+    };    //
+    if (value === 0x00) {
+        engine.scratchDisable(deckno);
+    };
+};
+
+
 KAOSSDJ.wheelTurn = function (channel, control, value, status, group) {
-    var newValue;
+    var deckno = channel - 6;
     if (value < 64) {
         newValue = value;
     } else {
         newValue = value - 128;
     }
-    if (engine.isScratching(KAOSSDJ.currentDeck)) {
-        engine.scratchTick(KAOSSDJ.currentDeck, newValue); // Scratch!
+    if (engine.isScratching(deckno)) {
+        engine.scratchTick(deckno, newValue); // Scratch!
     } else {
-        engine.setValue('[Channel'+KAOSSDJ.currentDeck+']', 'jog', newValue); // Pitch bend
+        engine.setValue('[Channel'+deckno+']', 'jog', newValue); // Pitch bend
     }
-}
+};
