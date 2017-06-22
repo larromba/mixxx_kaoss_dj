c636eb921632ae860a92e0741ce718780e9f7437
Fayaaz Ahmed <fayaaz.ahmed@gmail.com>
2017-06-22 03:09:48 +0100

    Jog wheels working now as described in manual

--- Korg-KAOSS-DJ-scripts.js
+++ Korg-KAOSS-DJ-scripts.js
@@ -4,7 +4,6 @@ function KAOSSDJ() {};
 
 
 KAOSSDJ.init = function (id, debugging) {
-    midi.sendShortMsg(0x97, 0x96, 0x7F)
     // turn on all LEDs
     for (var i = 0x00; i <= 0xFF; i++) {
         for(var n = 0xB6; n <= 0xB8; n++) {
@@ -12,14 +11,14 @@ KAOSSDJ.init = function (id, debugging) {
       };
     };
 };
+
 KAOSSDJ.shutdown = function (id, debugging) {
-    midi.sendShortMsg(0x97, 0x96, 0x7F)
-    // turn on all LEDs
-    for (var i = 0x00; i <= 0xFF; i++) {
-        for(var n = 0xB6; n <= 0xB8; n++) {
-            midi.sendShortMsg(n, i, 0x00);
-      };
+  // turn on all LEDs
+  for (var i = 0x00; i <= 0xFF; i++) {
+    for(var n = 0xB6; n <= 0xB8; n++) {
+      midi.sendShortMsg(n, i, 0x00);
     };
+  };
 };
 
 
@@ -46,49 +45,79 @@ var leds = {
 
 
 KAOSSDJ.deck = function(decknum) {
-    this.decknum = decknum;
-    this.group = "[Channel" + decknum + "]";
-    this.loaded = false;
-    this.jogWheelsInScratchMode = false;
-    this.PADMode = false; //false = not pressed; true = pressed
-    this.shiftKey = false;
-    this.touch = false;
-
+  this.decknum = decknum;
+  this.group = "[Channel" + decknum + "]";
+  this.loaded = false;
+  this.jogWheelsInScratchMode = false;
+  this.shiftKey = false;
+  this.touch = false;
 };
 
 
+KAOSSDJ.decks = {}
+// Initialise the 4 deck objects and their properties
+for (var i = 1; i <= 4; i++) {
+  KAOSSDJ.decks['D' + i] = new KAOSSDJ.deck(i);
+}
+
+
+KAOSSDJ.getDeckByChannel = function(channel){
+  var deckno = channel - 6;
+  return KAOSSDJ.decks['D' + deckno];
+};
+
+ KAOSSDJ.updateDeckByChannel = function(channel, key, value){
+   var deckno = channel - 6;
+   KAOSSDJ.decks['D' + deckno][key] = value;
+ };
+
 KAOSSDJ.currentDeck = function(channel){
-   script.midiDebug(channel);
-   var chann = (channel - 6);
-   print(chann);
-   return chann;
+ script.midiDebug(channel);
+ var chann = (channel - 6);
+ print(chann);
+ return chann;
 };
 
 KAOSSDJ.wheelTouch = function (channel, control, value, status, group) {
-    var alpha = 1.0/8;
-    var beta = alpha/32;
-    var deckno = channel - 6
-    print ('Deck num: ' + deckno);
+  var alpha = 1.0/8;
+  var beta = alpha/32;
 
+  var deck_info = KAOSSDJ.getDeckByChannel(channel);
+  var deckno = deck_info.decknum;
+
+  if (deck_info.jogWheelsInScratchMode) {
     if (value === 0X7F) {
         engine.scratchEnable(deckno, 128, 33+1/3, alpha, beta);
-    };    //
-    if (value === 0x00) {
+    }    //
+    else if (value === 0x00) {
         engine.scratchDisable(deckno);
     };
+  }
 };
 
 
 KAOSSDJ.wheelTurn = function (channel, control, value, status, group) {
-    var deckno = channel - 6;
-    if (value < 64) {
-        newValue = value;
-    } else {
-        newValue = value - 128;
-    }
-    if (engine.isScratching(deckno)) {
-        engine.scratchTick(deckno, newValue); // Scratch!
-    } else {
-        engine.setValue('[Channel'+deckno+']', 'jog', newValue); // Pitch bend
-    }
+  print(channel);
+  var deckno = channel - 6;
+  if (value < 64) {
+    newValue = value;
+  } else {
+    newValue = value - 128;
+  }
+  if (engine.isScratching(deckno)) {
+    engine.scratchTick(deckno, newValue); // Scratch!
+  } else {
+    engine.setValue('[Channel'+deckno+']', 'jog', newValue); // Pitch bend
+  }
 };
+
+KAOSSDJ.scratchMode = function(channel, control, value, status, group) {
+
+  if (value == 0x7F) {
+    print('Entering scratch mode')
+    KAOSSDJ.updateDeckByChannel(channel, 'jogWheelsInScratchMode', true);
+  }
+  else if (value == 0x00) {
+    KAOSSDJ.updateDeckByChannel(channel, 'jogWheelsInScratchMode', false);
+  }
+}
